633b7bcd4a4284c98d2145ceeba9ea972f08b3f1
cynthia nweke <nwekecynthia2015@gmail.com>
2022-10-11 09:21:06 +0100

    project work

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+var pagebody=document.querySelector(".page")
+var hamburger=document.querySelector(".hamburger-container")
+
+
+function add(){
+   hamburger.classList.toggle('showbutton') 
+}
+
+var mobile=document.querySelector(".mobile")
+
+function add(){
+    hamburger.classList.toggle('showbutton')
+    mobile.classList.toggle('showmobile')
+
+}
