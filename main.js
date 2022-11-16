dc32787e268ce71cb056261dad8622cd9225b322
cynthia nweke <nwekecynthia2015@gmail.com>
2022-11-16 08:48:24 +0100

    completed work

--- main.js
+++ main.js
@@ -17,3 +17,14 @@ function add(){
 
 }
 
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2000,
+    reset: true
+
+ })
+ sr.reveal('.first-div',{delay:350, origin:'leftt'})
+ sr.reveal('.first-rightdiv', {delay:350, origin:'right'})
+ sr.reveal('.second-div-text, .text-content,.section-text,.swiper-wrapper,.inspire-text', {delay:350, origin:'bottom'})
+
+       
